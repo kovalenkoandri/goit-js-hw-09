cbff886e95e77c45112b4e11aec3135377f79062
kovalenkoandri <kovalenkoandrii@gmail.com>
2022-07-28 12:45:43 +0300

    01 color-switcher done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,17 @@
 
+const refs = {};
+refs.btnStart = document.querySelector('button[data-start]');
+refs.btnStop = document.querySelector('button[data-stop]');
+refs.body = document.querySelector('body');
+let timerId = null;
+refs.btnStart.addEventListener('click', () => {
+    timerId = setInterval(() => refs.body.style.backgroundColor = getRandomHexColor(), 2000);
+    refs.btnStart.disabled = true;
+});
+refs.btnStop.addEventListener('click', () => {
+    clearInterval(timerId);
+    refs.btnStart.disabled = false;
+});
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
